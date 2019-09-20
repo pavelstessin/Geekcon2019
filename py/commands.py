fa5f32a1e4ff88a30dd366cdde4540cb08fc3e2e
Lior Neuman <liorn>
2019-09-21 01:37:02 +0300

    fix api after merge

--- py/commands.py
+++ py/commands.py
@@ -5,7 +5,7 @@ DEBUG = False
 
 def get_project_name(args):
     import switch_words
-    result = switch_words.get_project()
+    selected_title, selected_blurb = switch_words.get_project()
     code = HTTPStatus.OK
     content_type = 'text/plain'
-    return result, code, content_type
+    return selected_title, code, content_type
